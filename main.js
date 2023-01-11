ae1af5981e7c05141b6b9c6e83500f5f651c0ce0
RyzenMu <muruga13in2022@gmail.com>
2023-01-11 08:20:04 +0530

    initialized

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+// print selected color on console 
+
+function select(value) {
+    console.log(value);
+    let body = document.getElementsByTagName('body');
+    console.log(body[0]);
+    body[0].style.backgroundColor = value;
+}
+
+// print selected color on html page
+
+
+
+
